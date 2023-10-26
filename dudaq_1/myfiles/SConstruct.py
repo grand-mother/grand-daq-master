c410df4d04935749e49ad5957ef7821caf0fd2b0
Colley JM <jcolley@lpnhe.in2p3.fr>
2023-10-26 14:57:50 +0200

    improve docstring

--- dudaq_1/myfiles/SConstruct.py
+++ dudaq_1/myfiles/SConstruct.py
@@ -1,31 +1,45 @@
+# ################################
+#
+# "Makefile" of dudaq excutable with Scons builder manager
+#            see website of Scons : https://scons.org/
+#
+# Configuration:
+# --------------
+-
+#  1) Scons installation xith python3
+#     python3 pip -m install scons
+# 
+#  2) ARM compilation tools chain
+#     it's necessary to have le path of aarch64-linux-gnu-gcc compilator
+#     with my installation it's here
+#     . /home/grand/install/xilinx/SDK/2018.3/settings64.sh
+#     now C compilator "aarch64-linux-gnu-gcc" must be available in PATH
+# 
+#
+# Build executable dudaq:
+# ----------------------
+# in terminal , in the same directory of this file, ie "SConstruct.py"
+#    $ scons   
+#  
+# 
+# Clean build:
+# ------------
+#    $ scons -c
+#
+# ################################
+
 import os
 import subprocess
 
+# retrieve git informations
 cmd = "git rev-parse --abbrev-ref HEAD"
 branch = subprocess.getoutput(cmd)
+
 cmd = "git rev-parse --short  HEAD"
 sha_git = subprocess.getoutput(cmd)
 
 
-# it's necessary to have le path of aarch64-linux-gnu-gcc compilator
-# with my installation it's here
-# . /home/grand/install/xilinx/SDK/2018.3/settings64.sh
-# so
-
-# ################################
-#            WARNING
-# Before compilation add aarch64-linux-gnu-gcc in your path, 
-# aarch64-linux-gnu-gcc is installed with Xilink SDK
-#
-# to build, just 
-# $scons
-# 
-# to clean, just
-# $scons -c
-#
-# ################################
-
-
+# Define environnement and flag compilation
 #https://stackoverflow.com/questions/61164904/compiler-not-found-when-building-with-scons
 # => ENV = os.environ need to retrieve the path of aarch64-linux-gnu-gcc
 CC_val = 'aarch64-linux-gnu-gcc'
@@ -33,5 +47,5 @@ CC_val = 'gcc'
 env = Environment(ENV = os.environ, CC=CC_val, CCFLAGS=f'-Wall -O2 -fmessage-length=0 -MMD -MP -DVERSION={branch} -DGIT_SHA={sha_git} ')
 
 
+# Build application
 env.Program('dudaq_jmc', ['ad_shm.c','buffer.c','dudaq.c','monitor.c','scope.c'])
-
