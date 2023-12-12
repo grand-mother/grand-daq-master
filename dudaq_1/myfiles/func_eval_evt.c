a6ad06dc6802c7f8602e284fc0769972b8c4768b
luckyjim <jcolley@lpnhe.in2p3.fr>
2023-12-12 16:59:54 +0100

    add generic module to do evaluation of traces

--- dudaq_1/myfiles/func_eval_evt.c
+++ dudaq_1/myfiles/func_eval_evt.c
@@ -1,50 +1,139 @@
+/**
+ * \file
+ * \brief
+ *
+ * \author Colley Jean-Marc, CNRS/IN2P3/LPNHE, Paris
+ */
+
 #include "func_eval_evt.h"
 #include "ring_buffer_eval.h"
 
-int FEEV_cpt = 0;
-int FEEV_stop = 0;
+int G_cpt = 0;
+int G_running = 1; // if 0 stop all thread
 
-void FEEV_main (void  *p_data)
+/**
+ * \fn FuncEval_struct * FEEV_create(RingBufferEval_struct*, void*)
+ * \brief
+ *
+ * \param p_rbe
+ * \param p_eval
+ * \return
+ */
+FuncEval_struct* FEEV_create (RingBufferEval_struct *p_rbe, void *p_eval)
 {
-  RingBufferEval_struct *p_rbe = (RingBufferEval_struct *)p_data;
-  uint16_t nb_eval;
-  uint32_t idx_eval;
-  struct timespec tempo =
-    { .tv_nsec = 500000000 };
+   FuncEval_struct *p_feev = (FuncEval_struct*) malloc (sizeof(FuncEval_struct));
+   p_feev->p_rbe = p_rbe;
+   p_feev->p_eval = p_eval;
+   return p_feev;
+}
 
-  while (FEEV_stop == 0)
-  {
-    while (1)
-    {
-// we must do evaluation all evaluation without tempo between
+/**
+ * \fn void FEEV_delete(FuncEval_struct**)
+ * \brief
+ *
+ * \param pp_feev
+ * \return
+ */
+
+void FEEV_delete (FuncEval_struct **pp_feev)
+{
+   RBE_delete(&((*pp_feev)->p_rbe));
+   /* p_eval must be free before */
+   assert((*pp_feev)->p_eval == NULL);
+   free (*pp_feev);
+   *pp_feev = NULL;
+}
+
+/**
+ * \fn void FEEV_run(void*)
+ * \brief
+ *
+ * \param p_data
+ */
+int FEEV_run (void *p_args)
+{
+   FuncEval_struct *p_fe = (FuncEval_struct*) p_args;
+   RingBufferEval_struct *p_rbe = (RingBufferEval_struct*) p_fe->p_rbe;
+   void *p_eval = p_fe->p_eval;
+
+   uint16_t nb_eval;
+   uint16_t idx_eval_buffer;
+   uint32_t idx_eval_byte;
+   struct timespec tempo =
+      { .tv_nsec = 500000000 };
+
+   while (G_running)
+   {
+      /* evaluation of all events without tempo between*/
       mutex_lock (&p_rbe->mutex);
       nb_eval = p_rbe->nb_eval;
       mutex_unlock (&p_rbe->mutex);
       if (nb_eval > 0)
       {
-	idx_eval = (uint32_t) p_rbe->inext_eval * (uint32_t) p_rbe->size_buffer;
-	p_rbe->a_prob[idx_eval] = FEEV_eval (p_rbe->a_buffers[idx_eval]);
-	RFE_update_eval (p_rbe);
+	 idx_eval_buffer = p_rbe->inext_eval;
+	 /* index (in byte ) where start buffer to evaluate */
+	 idx_eval_byte = ((uint32_t) idx_eval_buffer) * p_rbe->size_buffer;
+	 /* eval and update ring buffer */
+	 FEEV_eval (p_eval, &(p_rbe->a_buffers[idx_eval_byte]), &(p_rbe->a_prob[idx_eval_buffer]));
+	 RFE_update_eval (p_rbe);
       }
       else
       {
-	break;
+	 /* all evaluation are done => sleep 0.1 ms */
+	 thrd_sleep (&tempo, NULL);
       }
-    }
-// all evaluation are done, => sleep 0.1 ms
-    thrd_sleep (&tempo, NULL);
-  }
+   }
+   return 0;
 }
 
-// fake eval
-float FEEV_eval (uint8_t *p_buf)
+/**
+ * \fn void FEEV_stop(void)
+ * \brief
+ *
+ */
+void FEEV_stop (void)
 {
-  FEEV_cpt++;
-  if (FEEV_cpt > 100)
-  {
-    FEEV_cpt = 0;
-    return 0.0;
-  }
-  return (FEEV_cpt / 100.0);
+   G_running = 0;
 }
 
+/**
+ * \fn float FEEV_eval(uint8_t*)
+ * \brief FAKE TRIGGER
+ *
+ * \param p_buf
+ * \return
+ */
+
+#ifdef FEEV_IS_FAKE
+
+void FEEV_eval (void *p_eval, uint8_t *p_buf, float *p_prob)
+{
+   G_cpt++;
+   if (G_cpt > 100)
+   {
+      G_cpt = 0;
+      return 0.0;
+   }
+   return (FEEV_cpt / 100.0);
+}
+#endif
+
+#define FEEV_IS_TFLT
+#ifdef FEEV_IS_TFLT
+
+#include "tflite_inference.h"
+/**
+ * \fn void FEEV_eval(void*, uint8_t*, float*)
+ * \brief Evaluation with neural network with tensorflow lite library
+ *
+ * \param p_eval
+ * \param p_buf
+ * \param p_prob
+ */
+void FEEV_eval (void *p_eval, uint8_t *p_buf, float *p_prob)
+{
+   TFLT_struct *p_tflt = (TFLT_struct*) p_eval;
+   TFLT_preprocessing (p_tflt, p_buf);
+   TFLT_inference (p_tflt, p_prob);
+}
+#endif
--- dudaq_1/myfiles/func_eval_evt.h
+++ dudaq_1/myfiles/func_eval_evt.h
@@ -1,5 +1,21 @@
+#ifndef DUDAQ_1_MYFILES_FUNC_EVAL_EVT_H_
+#define DUDAQ_1_MYFILES_FUNC_EVAL_EVT_H_
+
+
 #include "ring_buffer_eval.h"
 
-void FEEV_main(void  *p_data);
+typedef struct
+{
+      RingBufferEval_struct *p_rbe; // data
+      void *p_eval; // for processing
+} FuncEval_struct;
 
-float FEEV_eval (uint8_t *p_buf);
+FuncEval_struct *FEEV_create(RingBufferEval_struct *p_rbe, void *p_eval);
+
+void FEEV_run (void *p_args);
+
+void FEEV_stop (void);
+
+void FEEV_eval (void *p_eval, uint8_t *p_buf, float *p_prob);
+
+#endif
