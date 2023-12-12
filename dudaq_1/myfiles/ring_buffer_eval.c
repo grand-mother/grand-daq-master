5636f256595e59f5c2c4d7332a09c14e1c56030c
luckyjim <jcolley@lpnhe.in2p3.fr>
2023-12-12 16:59:02 +0100

    add module to manage ring buffer for evaluation/trigger T2

--- dudaq_1/myfiles/ring_buffer_eval.c
+++ dudaq_1/myfiles/ring_buffer_eval.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <bits/stdint-uintn.h>
 
-int RFE_error = 0;
+int RBE_error = 0;
 
 /**
- * \fn RingBufferEval_struct RFE_create*(uint16_t, uint16_t)
+ * \fn RingBufferEval_struct RBE_create*(uint16_t, uint16_t)
  * \brief
  *
  *
@@ -16,57 +16,57 @@ int RFE_error = 0;
  * \return
  */
 RingBufferEval_struct*
-RFE_create (uint16_t size_buffer, uint16_t nb_array)
+RBE_create (uint16_t size_buffer, uint16_t nb_array)
 {
-  RingBufferEval_struct *p_rbe;
-  uint64_t nb_byte;
-  uint8_t *p_buf;
-  float *p_prob;
-  int ret_mtx;
+   RingBufferEval_struct *p_rbe;
+   uint64_t nb_byte;
+   uint8_t *p_buf;
+   float *p_prob;
+   int ret_mtx;
 
-  p_rbe = (RingBufferEval_struct*) malloc (sizeof(RingBufferEval_struct));
-  if (p_rbe == NULL)
-  {
-    RFE_error = 1;
-    return NULL;
-  }
-  p_rbe->inext_eval = 0;
-  p_rbe->inext_trig = 0;
-  p_rbe->inext_write = 0;
-  p_rbe->nb_array = nb_array;
-  p_rbe->idx_max = nb_array - 1;
-  p_rbe->size_buffer = size_buffer;
-  p_rbe->nb_eval = 0;
-  p_rbe->nb_trig = 0;
-  p_rbe->nb_write = nb_array;
-  // alloc array of buffer
-  nb_byte = (uint64_t) size_buffer * (uint64_t) nb_array;
-  p_buf = (uint8_t*) malloc (nb_byte);
-  if (p_buf == NULL)
-  {
-    RFE_error = 2;
-    return NULL;
-  }
-  p_rbe->a_buffers = p_buf;
-  p_prob = (float*) malloc (sizeof(float) * (uint64_t) nb_array);
-  if (p_prob == NULL)
-  {
-    RFE_error = 3;
-    return NULL;
-  }
-  p_rbe->a_prob = p_prob;
-  ret_mtx = mtx_init (&(p_rbe->mutex), mtx_plain);
-  if (ret_mtx != thrd_success)
-  {
-    RFE_error = 4;
-    return NULL;
-  }
+   p_rbe = (RingBufferEval_struct*) malloc (sizeof(RingBufferEval_struct));
+   if (p_rbe == NULL)
+   {
+      RBE_error = 1;
+      return NULL;
+   }
+   p_rbe->inext_eval = 0;
+   p_rbe->inext_trig = 0;
+   p_rbe->inext_write = 0;
+   p_rbe->nb_array = nb_array;
+   p_rbe->idx_max = nb_array - 1;
+   p_rbe->size_buffer = size_buffer;
+   p_rbe->nb_eval = 0;
+   p_rbe->nb_trig = 0;
+   p_rbe->nb_write = nb_array;
+   // alloc array of buffer
+   nb_byte = (uint64_t) size_buffer * (uint64_t) nb_array;
+   p_buf = (uint8_t*) malloc (nb_byte);
+   if (p_buf == NULL)
+   {
+      RBE_error = 2;
+      return NULL;
+   }
+   p_rbe->a_buffers = p_buf;
+   p_prob = (float*) malloc (sizeof(float) * (uint64_t) nb_array);
+   if (p_prob == NULL)
+   {
+      RBE_error = 3;
+      return NULL;
+   }
+   p_rbe->a_prob = p_prob;
+   ret_mtx = mtx_init (&(p_rbe->mutex), mtx_plain);
+   if (ret_mtx != thrd_success)
+   {
+      RBE_error = 4;
+      return NULL;
+   }
 
-  return p_rbe;
+   return p_rbe;
 }
 
 /**
- * \fn uint8_t RFE_delete(RingBufferEval_struct**)
+ * \fn uint8_t RBE_delete(RingBufferEval_struct**)
  * \brief
  *
  *
@@ -74,87 +74,107 @@ RFE_create (uint16_t size_buffer, uint16_t nb_array)
  * \param p_rbe
  * \return
  */
-uint8_t RFE_delete (RingBufferEval_struct **pp_rbe)
+void RBE_delete (RingBufferEval_struct **pp_rbe)
 {
-  free ((*pp_rbe)->a_buffers);
-  free ((*pp_rbe)->a_prob);
-  free (*pp_rbe);
-  *pp_rbe = NULL;
-  return 0;
+   free ((*pp_rbe)->a_buffers);
+   free ((*pp_rbe)->a_prob);
+   mtx_destroy (&((*pp_rbe)->mutex));
+   free (*pp_rbe);
+   *pp_rbe = NULL;
 }
 
 /**
- * \fn void RFE_update_write(RingBufferEval_struct*)
+ * \fn void RBE_write(RingBufferEval_struct*, void *p_buf)
  * \brief
  *
  *
  *
  * \param p_rbe
  */
-inline void RFE_update_write (RingBufferEval_struct *p_rbe)
+inline void RBE_write (RingBufferEval_struct *p_rbe, void *p_buf)
 {
-  mutext_lock (&p_rbe->mutex);
-  p_rbe->nb_write -= 1;
-  p_rbe->nb_eval += 1;
-  RFE_inc_modulo (&p_rbe->inext_write, p_rbe->idx_max);
-  mutext_unlock (&p_rbe->mutex);
+   uint32_t idx_eval_buffer = p_rbe->inext_eval;
+   /* index (in byte ) where start buffer to evaluate */
+   uint32_t idx_eval_byte = ((uint32_t) idx_eval_buffer) * p_rbe->size_buffer;
+   memcpy(&p_rbe->a_buffers[idx_eval_byte], p_buf, p_rbe->size_buffer);
+   p_rbe->nb_write -= 1;
+   p_rbe->nb_eval += 1;
+   RBE_inc_modulo (&p_rbe->inext_write, p_rbe->idx_max);
 }
 
 /**
- * \fn void RFE_update_eval(RingBufferEval_struct*)
+ * \fn void RBE_update_write(RingBufferEval_struct*)
  * \brief
  *
  *
  *
  * \param p_rbe
  */
-inline void RFE_update_eval (RingBufferEval_struct *p_rbe)
+inline void RBE_update_write (RingBufferEval_struct *p_rbe)
 {
-  mutext_lock (&p_rbe->mutex);
-  p_rbe->nb_eval -= 1;
-  p_rbe->nb_trig += 1;
-  RFE_inc_modulo (&p_rbe->inext_eval, p_rbe->idx_max);
-  mutext_unlock (&p_rbe->mutex);
+   mutext_lock (&p_rbe->mutex);
+   p_rbe->nb_write -= 1;
+   p_rbe->nb_eval += 1;
+   RBE_inc_modulo (&p_rbe->inext_write, p_rbe->idx_max);
+   mutext_unlock (&p_rbe->mutex);
 }
 
 /**
- * \fn void RFE_update_trigger(RingBufferEval_struct*)
+ * \fn void RBE_update_eval(RingBufferEval_struct*)
  * \brief
  *
  *
  *
  * \param p_rbe
  */
-inline void RFE_update_trigger (RingBufferEval_struct *p_rbe)
+inline void RBE_update_eval (RingBufferEval_struct *p_rbe)
 {
-  mutext_lock (&p_rbe->mutex);
-  p_rbe->nb_trig -= 1;
-  p_rbe->nb_write += 1;
-  RFE_inc_modulo (&p_rbe->inext_trig, p_rbe->idx_max);
-  mutext_unlock (&p_rbe->mutex);
+   mutext_lock (&p_rbe->mutex);
+   p_rbe->nb_eval -= 1;
+   p_rbe->nb_trig += 1;
+   RBE_inc_modulo (&p_rbe->inext_eval, p_rbe->idx_max);
+   mutext_unlock (&p_rbe->mutex);
 }
 
 /**
- * \fn uint16_t RFE_inc_modulo(uint16_t*)
+ * \fn void RBE_update_trigger(RingBufferEval_struct*)
+ * \brief
+ *
+ *
+ *
+ * \param p_rbe
+ */
+inline void RBE_update_trigger (RingBufferEval_struct *p_rbe)
+{
+   mutext_lock (&p_rbe->mutex);
+   p_rbe->nb_trig -= 1;
+   p_rbe->nb_write += 1;
+   RBE_inc_modulo (&p_rbe->inext_trig, p_rbe->idx_max);
+   mutext_unlock (&p_rbe->mutex);
+}
+
+/**
+ * \fn uint16_t RBE_inc_modulo(uint16_t*)
  * \brief
  *
  *
  * \param p_int
  * \return
  */
-inline void RFE_inc_modulo (uint16_t *p_int, uint16_t max_int)
+
+inline void RBE_inc_modulo (uint16_t *p_int, uint16_t max_int)
 {
-  if (*p_int == max_int)
-  {
-    *p_int = 0;
-  }
-  else
-  {
-    *p_int += 1;
-  }
+   if (*p_int == max_int)
+   {
+      *p_int = 0;
+   }
+   else
+   {
+      *p_int += 1;
+   }
 }
 
-void RFE_print_error (void)
+void RBE_print_error (void)
 {
-  printf ("\nRFE module : error %d (O is OK)", RFE_error);
+   printf ("\nRFE module : error %d (O is OK)", RBE_error);
 }
--- dudaq_1/myfiles/ring_buffer_eval.h
+++ dudaq_1/myfiles/ring_buffer_eval.h
@@ -3,53 +3,57 @@
  * \brief
  *
  */
+#ifndef DUDAQ_1_MYFILES_RING_BUFFER_H_
+#define DUDAQ_1_MYFILES_RING_BUFFER_H_
 
 #include <bits/stdint-uintn.h>
 #include  <threads.h>
 
 typedef struct
 {
-// dimension of array of buffer
-    uint16_t size_buffer;// size of one buffer in byte
-    uint16_t nb_array;// number of buffer in array
-    uint16_t idx_max;// index max , ie nb_array-1
+      /* dimension of array of buffer */
+      uint16_t size_buffer; // size of one buffer in byte
+      uint16_t nb_array; // number of buffer in array
+      uint16_t idx_max; // index max , ie nb_array-1
 
-// index in array, nb_array > inext_trig >= 0
-    uint16_t inext_write;// index next write
-    uint16_t inext_eval;// index next evaluation
-    uint16_t inext_trig;// index next trigger
+      /* index in array, nb_array > inext_trig >= 0*/
+      uint16_t inext_write; // index next write
+      uint16_t inext_eval; // index next evaluation
+      uint16_t inext_trig; // index next trigger
 
-// number of buffer
-    uint16_t nb_write;//nb buffer available
-    uint16_t nb_eval;// nb buffer to evaluate
-    uint16_t nb_trig;// nb buffer to trig
+      /* number of buffer */
+      /* nb_write+nb_eval+nb_trig=nb_array*/
+      uint16_t nb_write; //nb buffer available
+      uint16_t nb_eval; // nb buffer to evaluate
+      uint16_t nb_trig; // nb buffer to trig
 
-// array of buffer
-    uint8_t *a_buffers;// array of all buffers
-    float *a_prob; // array of probability given by evaluation
+      /* array of buffer*/
+      uint8_t *a_buffers; // array of all buffers
+      float *a_prob; // array of probability for each buffer
 
-// mutex
-    mtx_t mutex;
+      /* mutex */
+      mtx_t mutex;
 } RingBufferEval_struct;
 
 RingBufferEval_struct*
-RFE_create (uint16_t size_buffer, uint16_t nb_array);
-
-uint8_t
-RFE_delete (RingBufferEval_struct **pp_rbe);
-
-inline void
-RFE_update_write (RingBufferEval_struct *p_rbe);
-
-inline void
-RFE_update_eval (RingBufferEval_struct *p_rbe);
-
-inline void
-RFE_update_trigger (RingBufferEval_struct *p_rbe);
-
-inline void
-RFE_inc_modulo(uint16_t *p_int, uint16_t max_int);
+RBE_create (uint16_t size_buffer, uint16_t nb_array);
 
 void
-RFE_print_error(void);
+RBE_delete (RingBufferEval_struct **pp_rbe);
 
+inline void
+RBE_update_write (RingBufferEval_struct *p_rbe);
+
+inline void
+RBE_update_eval (RingBufferEval_struct *p_rbe);
+
+inline void
+RBE_update_trigger (RingBufferEval_struct *p_rbe);
+
+inline void
+RBE_inc_modulo (uint16_t *p_int, uint16_t max_int);
+
+void
+RBE_print_error (void);
+
+#endif
