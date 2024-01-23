c63ca315a89aeab680421ac00816461b6f1da538
Colley JM <jcolley@lpnhe.in2p3.fr>
2024-01-23 16:00:06 +0100

    remove test_sum example

--- dudaq_1/tests/makefile_scons.py
+++ dudaq_1/tests/makefile_scons.py
@@ -8,17 +8,11 @@ CPPUTEST_HOME = "/home/grand/install/cpputest/include"
 PATH_TF = "/home/grand/install/tf_lite/tensorflow_src"
 
 
-# Build cpputest_example
-if False:
-	src_files = Split('test_my_sum.cpp my_sum.c run_cpputest.cpp ')
-	env.Program('run_cpputest', src_files, 
-		LIBS = ['CppUTest','CppUTestExt'],
-		LIBPATH = "/home/grand/install/cpputest/cpputest_build/lib")
-
 # sources
 src_prj = glob.glob(f"{PATH_SRC}*.c")
 print(src_prj)
-src_all = ['test_ring_buffer_eval.cpp', 'run_cpputest.cpp','test_tflite_inference.cpp','test_func_eval.cpp'] + src_prj
+src_tests = ['test_ring_buffer_eval.cpp', 'run_cpputest.cpp','test_tflite_inference.cpp','test_func_eval.cpp']
+src_all = src_tests + src_prj
 
 
 # compilation option
