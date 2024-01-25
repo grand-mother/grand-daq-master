2242c987d993e47bad7bf3e6dca9f0600719c604
Colley JM <jcolley@lpnhe.in2p3.fr>
2024-01-25 13:29:41 +0100

    add module test

---
+++ dudaq_1/tests/test_data.h
@@ -0,0 +1,18 @@
+/*
+ * test_data.h
+ *
+ *  Created on: Jan 23, 2024
+ *      Author: grand
+ */
+
+#ifndef TEST_DATA_H_
+#define TEST_DATA_H_
+
+#define NB_SAMPLE 1024
+
+extern float trace_1[NB_SAMPLE];
+extern float trace_2[NB_SAMPLE];
+extern float trace_3[NB_SAMPLE];
+
+
+#endif /* TEST_DATA_H_ */
